93964a6a2e9065db2545085a4a6e19997ade06ad
JacobBraxley <jacob@roboticsys.com>
2019-03-27 17:41:27 -0400

    Getting closer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,68 +10,109 @@ const TriviaQuestion = class  TriviaItem {
     }
 }
 
+var currentlyShowing = false;
+
+function questionTime(show = true) {
+    if(currentlyShowing != show) {
+        if(show) {
+            $("#start").addClass("d-none");
+            $(".timeSection").removeClass("d-none");
+            $(".question").removeClass("d-none");
+            $(".option1").removeClass("d-none");
+            $(".option2").removeClass("d-none");
+            $(".option3").removeClass("d-none");
+            $(".option4").removeClass("d-none");
+        } else {
+            $("#start").removeClass("d-none");
+            $(".timeSection").addClass("d-none");
+            $(".question").addClass("d-none");
+            $(".option1").addClass("d-none");
+            $(".option2").addClass("d-none");
+            $(".option3").addClass("d-none");
+            $(".option4").addClass("d-none");
+        }
+    }
+}
+
 var triviaGame = {
     correctAnswers: 0,
     incorrectAnswers: 0,
     unanswered: 0,
 
-    timeoutForAQuestion: 30,
-    timeTilNextQuestion: 4,
+    timeoutForAQuestion: 4,
+    timeTilNextQuestion: 2,
 
     questions: [],
-    questionIndex: 0,
+    questionIndex: -1,
+
+    timer: undefined,
 
     guess: function(guess) {
-        //TODO - cancel Timeout
-        var right = questions[questionIndex].evaluateGuess(guess);
+        clearInterval(this.timer);
+        var right = this.questions[this.questionIndex].evaluateGuess(guess);
         if(right) {
-            correctAnswers++;
+            this.correctAnswers++;
             //TODO - Video
+            setTimeout(this.loadNextQuestion, this.timeTilNextQuestion * 1000);
         } else {
-            incorrectAnswers++;
+            this.incorrectAnswers++;
             //TODO - Video
+            setTimeout(this.loadNextQuestion, this.timeTilNextQuestion * 1000);
         }
         loadNextQuestion();
     },
 
-    timeout: function() {
-        unanswered++;
-        loadNextQuestion();
+    tick: function() {
+        var time = Number($("#timeLeft").text()) - 1;
+        $("#timeLeft").text(time);
+
+        if(time <= 0) { //timeout, fail condition.
+            debugger;
+            clearInterval(this.timer);
+            this.unanswered++;
+            //TODO - load timeout video.
+            setTimeout(this.loadNextQuestion, this.timeTilNextQuestion * 1000);
+        }
     },
 
     loadNextQuestion: function() {
-        //TODO - domUpdates
-        //TODO - start Timer
-        if(++this.questionIndex == this.questions.length) {
+        questionTime(true);
+        //Boring but we're just going to keep iterating through the list.
+        this.questionIndex++;
+        if(this.questionIndex >= this.questions.length) {
             this.triggerEndScreen();
         }
-
+        else {
+            $("#timeLeft").text(this.timeoutForAQuestion);
+            $(".question").text(this.questions[this.questionIndex].question);
+            $(".option1").text(this.questions[this.questionIndex].possibleAnswers[0]);
+            $(".option2").text(this.questions[this.questionIndex].possibleAnswers[1]);
+            $(".option3").text(this.questions[this.questionIndex].possibleAnswers[2]);
+            $(".option4").text(this.questions[this.questionIndex].possibleAnswers[3]);
+           
+            this.timer = setInterval(this.tick, 1000);
+            if(++this.questionIndex == this.questions.length) {
+                this.triggerEndScreen();
+            }
+        }
     },
 
     triggerEndScreen: function() {
-        //TODO - domUpdates
+        questionTime(false);
+
     },
 
     resetGame: function() {
         this.correctAnswers = 0;
         this.incorrectAnswers = 0;
         this.unanswered = 0;
-        this.questionIndex = 0; 
+        this.questionIndex = -1; 
         loadNextQuestion();
     }
 }
 
 //Question Definition Area
-// jumbotron,  Which of the component is used for a primary display area?   bigscreen, jumbotron, maindisplay, div
-// container, Which of the following is used to define a bootstrap grid?  box, grid, container, object
-// animate,  Which do you use to change the style of an item from one state to another? animate, new, change, overwrite
-// shadowbox Which class do you include to create shadow boxes with Bootstrap?  fade, depth, shadow, borderbox
-// media sizing differences Which of the following do you use to control the look of your website for different devices? @display, @media, @displaysize, @query
-// play video Which do you use to add a video to your website?  htmltube, media, play, video
-// flexbox, Which of the following display types should you use to make a responsive element?  flex, block, inline, none
-// class, Which of the following should you use to select a class? #, @, ., $
-
-function resetQuestions() {
+function setQuestions() {
     triviaGame.questions = [
         new TriviaQuestion("Which of the component is used for a primary display area?", 1, ["bigscreen", "jumbotron", "maindisplay", "div"]),
         new TriviaQuestion("Which of the following is used to define a bootstrap grid?", 2, ["box", "grid", "container", "object"]),
@@ -83,4 +124,9 @@ function resetQuestions() {
         new TriviaQuestion("Which of the following should you use to select a class?", 2, ["#", "@", ".", "$"])
     ];
 }
-//dom .on("click", ) Area.
+setQuestions();  //There isn't much use for defining the function at this time.
+
+//dom events
+$("#start").on("click", function() {
+    triviaGame.loadNextQuestion();
+});
